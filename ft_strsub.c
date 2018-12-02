b4c331d7bf208c77c087b00492ecb1dbe20da6cc
Eric LAZO <erlazo@e1r9p2.42.fr>
2018-12-02 21:47:27 +0100

    good modifications for now

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: erlazo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 17:04:32 by erlazo            #+#    #+#             */
-/*   Updated: 2018/11/14 17:50:19 by erlazo           ###   ########.fr       */
+/*   Updated: 2018/12/02 21:46:55 by erlazo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 
 	a = 0;
 	if (!s)
-		return (0);
-	if (!(ret = (char*)malloc(sizeof(char) * (len + 1))))
+		return (0);				// or NULL ???
+	if (!(ret = (char*)malloc(sizeof(char) * (len + 1))))			// memalloc ????
 		return (0);
 	while (a < len)
 	{
