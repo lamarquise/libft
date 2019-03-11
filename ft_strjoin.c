120453fd13db35b9fbef471a734896249958308e
Eric LAZO <erlazo@e3r7p23.42.fr>
2019-03-11 19:33:53 +0100

    finally a strjoin that works

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,38 +6,33 @@
 /*   By: erlazo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 17:13:52 by erlazo            #+#    #+#             */
-/*   Updated: 2019/03/06 17:09:59 by erlazo           ###   ########.fr       */
+/*   Updated: 2019/03/08 17:09:46 by erlazo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		l(char *str)
-{
-	int		a;
-
-	a = 0;
-	while (str[a])
-		++a;
-	return ((a - 1) < 0 ? 0 : a - 1);
-}
-
 char			*ft_strjoin(char const *s1, char const *s2)
 {
 	int		a;
-	int		b;
 	char	*ret;
 
 	a = 0;
-	b = 0;
 	if (!s1 || !s2)
 		return (NULL);
 	if (!(ret = (char*)ft_memalloc(sizeof(char)
-		* l((char*)s1) + l((char*)s2) + 1)))
+		* (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (0);
-	if (!(ret = ft_strdup(s1)))
-		return (NULL);
-	if (!(ret = ft_strcat(ret, s2)))
-		return (NULL);
+	while (*s1)
+	{
+		ret[a++] = *s1;
+		++s1;
+	}
+	while (*s2)
+	{
+		ret[a++] = *s2;
+		++s2;
+	}
+	ret[a] = '\0';
 	return (ret);
 }
